a5b368801335e7efc23ae2a16c1318ee3142391f
Borna Koružnjak <borna@getizzy.com>
2016-12-07 11:09:52 +0100

    - questions are now being displayed normally

--- src/iQuestions.java
+++ src/iQuestions.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-/** Created by Nav on 6/12/16. **/
+/**
+ * Created by Nav on 6/12/16.
+ **/
 public class iQuestions {
     // Declared Variables
     private Scanner scanner = new Scanner(System.in);
@@ -30,8 +32,8 @@ public class iQuestions {
     }
 
     // Adds new question to array list
-    private boolean addQuestion(String question, String optionA, String optionB, String optionC, String optionD, char correctOption, String messageOnAnswering, boolean isDisplayed){
-        if(findQuestions(this.iQuestions.size() + 1) == null){
+    private boolean addQuestion(String question, String optionA, String optionB, String optionC, String optionD, char correctOption, String messageOnAnswering, boolean isDisplayed) {
+        if (findQuestions(this.iQuestions.size() + 1) == null) {
             this.iQuestions.add(new iQuestionStorage(this.iQuestions.size() + 1, question, optionA, optionB, optionC, optionD, correctOption, messageOnAnswering, isDisplayed));
             return true;
         }
@@ -44,16 +46,16 @@ public class iQuestions {
         this.iQuestions = new ArrayList<iQuestionStorage>();
     }
 
-    private iQuestionStorage findQuestions(int uniqueID){
-        for (iQuestionStorage checked: iQuestions){
-            if(uniqueID == checked.getUniqueID()){
+    private iQuestionStorage findQuestions(int uniqueID) {
+        for (iQuestionStorage checked : iQuestions) {
+            if (uniqueID == checked.getUniqueID()) {
                 return checked;
             }
         }
         return null;
     }
 
-    public void addQuestions(){
+    public void addQuestions() {
         addQuestion("Test1", "1", "2", "3", "4", 'A', "Working", false);
         addQuestion("Test2", "1", "2", "3", "4", 'A', "Working", false);
         addQuestion("Test3", "1", "2", "3", "4", 'A', "Working", false);
@@ -64,54 +66,74 @@ public class iQuestions {
         addQuestion("Test8", "1", "2", "3", "4", 'A', "Working", false);
     }
 
-    public void checkSize(){
+    public void checkSize() {
         System.out.println(this.iQuestions.size());
     }
 
-    public void displayQuestion(){
-        for (int i = 0; i < this.iQuestions.size(); i++){
-            displayQuestion(randomNumberGenerator());
+    public void displayQuestion() {
+        iQuestions = shuffleQuestions(iQuestions);
+        for (int i = 0; i < this.iQuestions.size(); i++) {
+            displayQuestion(i);
         }
     }
 
-    private void displayQuestion(int number){
-        for (int i = 0; i < iQuestions.size(); i++){
-            iQuestionStorage storage = this.iQuestions.get(i);
-            if(number == storage.getUniqueID() && storage.isDisplayed() == false){
-                checkQuestion(storage.getQuestion(), storage.getOptionA(), storage.getOptionB(), storage.getOptionC(), storage.getOptionD(), storage.getCorrectOption(), storage.getMessageOnAnswering(), storage.isDisplayed());
-                storage.setDisplayed(true);
-            }
+    private void displayQuestion(int number) {
+        iQuestionStorage questionToDisplay = this.iQuestions.get(number);
+        if (!questionToDisplay.isDisplayed()) {
+            checkQuestion(questionToDisplay.getQuestion(), questionToDisplay.getOptionA(), questionToDisplay.getOptionB(), questionToDisplay.getOptionC(), questionToDisplay.getOptionD(), questionToDisplay.getCorrectOption(), questionToDisplay.getMessageOnAnswering(), questionToDisplay.isDisplayed());
+            questionToDisplay.setDisplayed(true);
         }
     }
 
-    private int randomNumberGenerator(){
+    private int randomNumberGenerator() {
         Random rand = new Random();
         int randomNumber = rand.nextInt((iQuestions.size() - 1) + 1);
         return randomNumber;
     }
 
-    private void checkQuestion(String question, String optionA, String optionB, String optionC, String optionD, char correctOption, String messageOnAnswering, boolean isDisplayed){
+    private void checkQuestion(String question, String optionA, String optionB, String optionC, String optionD, char correctOption, String messageOnAnswering, boolean isDisplayed) {
         char option;
         System.out.println("Player Name: " + this.playerName + " | " + "Points: " + this.points + " | " + " Tries Left: " + this.triesLeft);
         System.out.println(question);
         System.out.println("A: " + optionA + "\n" + "B: " + optionB + "\n" + "C: " + optionC + "\n" + "D: " + optionD);
         do {
-            option = this.scanner.next().charAt(0);
-            if(option == correctOption){
+            option = this.scanner.next().toUpperCase().charAt(0);
+            if (option == correctOption) {
                 continue;
-            }else{
-                if (triesLeft > 0 && points > 0){
+            } else {
+                if (triesLeft > 0 && points > 0) {
                     setPoints(getPoints() - 5);
                     setTriesLeft(getTriesLeft() - 1);
-                }else{
-                    if(getPoints() < 0 || getTriesLeft() < 0){
+                } else {
+                    if (getPoints() < 0 || getTriesLeft() < 0) {
                         System.out.println("You cannot continue as you are out of points or tries");
-                    }else{
+                    } else {
                         System.out.println("You cannot continue as you are out of points or tries");
                     }
                 }
             }
-        }while(option != correctOption);
+        } while (option != correctOption);
         System.out.println(messageOnAnswering);
+    }
+
+    private ArrayList<iQuestionStorage> shuffleQuestions(ArrayList<iQuestionStorage> questionDatabase) {
+        if (questionDatabase == null) {
+            return questionDatabase;
+        }
+
+        if (questionDatabase.isEmpty()) {
+            return questionDatabase;
+        }
+
+        int n = questionDatabase.size();
+        Random random = new Random();
+        random.nextInt();
+        for (int i = 0; i < n; i++) {
+            int change = i + random.nextInt(n - i);
+            iQuestionStorage helper = questionDatabase.get(i);
+            questionDatabase.set(i, questionDatabase.get(change));
+            questionDatabase.set(change, helper);
+        }
+        return questionDatabase;
     }
 }
